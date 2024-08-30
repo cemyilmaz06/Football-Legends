10e4046435b9967e8d0e1aeb5934f424de6f2577
cem <cemyilmaztech@gmail.com>
2024-08-30 19:37:57 +0300

    LegendCard

--- src/components/legends/LegendCard.jsx
+++ src/components/legends/LegendCard.jsx
@@ -1,36 +1,27 @@
 import { useState } from "react";
 import { Card, CardImg, Container, Image } from "react-bootstrap";
 
-const LegendCard=({img,name,statistics})=>{
-    const [show, setShow] = useState(true)
-    return(
-        <>
-<Container>
-<Card role="button" onClick={()=>setShow(!show)}>
-    {
-    show ? ( <Image src={img} rounded />) : ( <Card.Body>{statistics.map((item)=>(
-        <ul><li>⚽{item}</li></ul>
-    ))}</Card.Body>)
-    }
-  
-   
-  
-   
-</Card>
-</Container>
-        
-        </>
-    )
-}
-
-
-
-
-
-
-
-
-
-
+const LegendCard = ({ img, name, statistics }) => {
+  const [show, setShow] = useState(true);
+  return (
+    <>
+      <Container>
+        <Card role="button" onClick={() => setShow(!show)}>
+          {show ? (
+            <Image src={img} rounded />
+          ) : (
+            <Card.Body>
+              {statistics.map((item) => (
+                <ul>
+                  <li>⚽{item}</li>
+                </ul>
+              ))}
+            </Card.Body>
+          )}
+        </Card>
+      </Container>
+    </>
+  );
+};
 
 export default LegendCard;
